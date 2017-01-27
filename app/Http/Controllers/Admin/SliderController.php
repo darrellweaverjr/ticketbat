295e52b65cb1f5e9a0dc1e59a1cf823d35fc5d4f
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-01-27 15:07:25 -0800

    add sliders

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+use App\Http\Models\Slider;
+use App\Http\Models\Image;
 
 /**
  * Manage Sliders
@@ -13,7 +16,29 @@ class SliderController extends Controller{
     
     public function index()
     {
-              
+        try {   
+            //init
+            $input = Input::all(); 
+            if(isset($input) && isset($input['id']))
+            {
+                //get selected record 
+                $slider = Slider::where('id','=',$input['id'])->first();
+                if(!$slider)
+                    return ['success'=>false,'msg'=>'There was an error getting the slider.<br>Maybe it is not longer in the system.'];
+                return ['success'=>true,'slider'=>$slider];
+            }
+            else
+            {      
+                //get all records        
+                $sliders = Slider::orderBy('sliders.n_order')->get();
+                foreach ($sliders as $s)
+                    $s->image_url = Image::view_image($s->image_url);
+                //return view
+                return view('admin.sliders.index',compact('sliders'));
+            }
+        } catch (Exception $ex) {
+            throw new Exception('Error Sliders Index: '.$ex->getMessage());
+        }
     }
     
 }
