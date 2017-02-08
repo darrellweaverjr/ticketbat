428c52d06640f97361b04446da5381eb20df5761
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-02-08 14:55:34 -0800

    show consignment purchases on purchases list as a method

--- resources/views/admin/purchases/index.blade.php
+++ resources/views/admin/purchases/index.blade.php
@@ -77,7 +77,8 @@
                                     <div class="search-content" >       
                                         <b class="search-title"><a data-toggle="modal" href="#modal_details_{{$p->id}}">@if($p->card_holder) {{$p->card_holder}} @else {{$p->first_name}} {{$p->last_name}} @endif</a></b>
                                         <br><small><i>Email: <a href="mailto:{{$p->email}}" target="_top">{{$p->email}}</a> ID: <a>{{$p->id}}</a> Tickets: <a>{{$p->quantity}}</a> Ticket Type: <a>{{$p->ticket_type_type}}</a> Package: <a>{{$p->title}}</a> 
-                                        @if($previous_color != $color) <br> Retail Price: <a>${{number_format($p->retail_price,2)}}</a> Fees: <a>${{number_format($p->processing_fee,2)}}</a> Commission: <a>${{number_format($p->commission_percent,2)}}</a> Savings: <a>${{number_format($p->savings,2)}}</a> Method: <a>{{$p->payment_type}}</a> @endif
+                                        @if($previous_color != $color) <br> Ret.Price: <a>${{number_format($p->retail_price,2)}}</a> Fees: <a>${{number_format($p->processing_fee,2)}}</a> Commiss.: <a>${{number_format($p->commission_percent,2)}}</a> 
+                                        Savings: <a>${{number_format($p->savings,2)}}</a> Method: <a>@if($p->ticket_type=='Consignment'){{$p->ticket_type}}@else{{$p->payment_type}}@endif</a> @endif
                                         <br><b>NOTE: </b><span id="note_{{$p->id}}">@php echo $p->note @endphp<span></i></small>
                                     </div>
                                 </td>
@@ -139,7 +140,7 @@
                                                                 <span class="arrow"> </span>
                                                                 <span class="body" style="height:50px"> 
                                                                     <div class="col-md-6"> ID:<b>{{$p->transaction_id}}</b><br> AuthCode: <b>{{$p->authcode}}</b><br> RefNum: <b>{{$p->refnum}}</b></div>
-                                                                    <div class="col-md-6"> Payment: <b>{{$p->payment_type}}</b><br> Card: <b>...{{$p->last_4}}</b></div> 
+                                                                    <div class="col-md-6"> Payment: <b>@if($p->ticket_type=='Consignment'){{$p->ticket_type}}@else{{$p->payment_type}}@endif</b><br> Card: <b>...{{$p->last_4}}</b></div> 
                                                                 </span>
                                                             </div>
                                                         </li>
