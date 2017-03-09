2455384df2b952034dbbe52866c6f9c244c00f40
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-03-09 11:54:59 -0800

    methods for app

--- app/Http/Controllers/App/AppController.php
+++ app/Http/Controllers/App/AppController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers\App;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Http\Models\Image;
-use App\Http\Models\Show;
-use App\Http\Models\Venue;
+use App\Http\Models\ShowTime;
 
 /**
  * Manage Users
@@ -37,6 +36,7 @@ class AppController extends Controller{
      */
     public function shows($id=null,$venue_id=null)
     {
+        $current = date('Y-m-d');
         if(!empty($id) && is_numeric($id))
         {
             $shows = DB::table('shows')
@@ -44,10 +44,32 @@ class AppController extends Controller{
                     ->join('images', 'show_images.image_id', '=' ,'images.id')
                     ->join('venues', 'venues.id', '=' ,'shows.venue_id')
                     ->join('locations', 'locations.id', '=' ,'venues.location_id')
-                    ->select('shows.id','shows.venue_id','shows.name','images.url','locations.city')
-                    ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('images.image_type','=','Logo')
+                    ->select('shows.id','shows.name','images.url','shows.description','shows.slug')
+                    ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('images.image_type','=','Header')
                     ->whereNotNull('images.url')->where('shows.id','=',$id)
                     ->distinct()->get(); 
+            $showtimes = ShowTime::where('show_id','=',$id)->where('show_time','>',$current)->orderBy('show_time')->take(50)->get(['id','show_time']); 
+            $videos = DB::table('shows')
+                        ->join('show_videos', 'show_videos.show_id', '=' ,'shows.id')
+                        ->join('videos', 'show_videos.video_id', '=' ,'videos.id')
+                        ->select('videos.id','videos.embed_code')
+                        ->where('shows.id','=',$id)
+                        ->distinct()->get();
+            $images = DB::table('shows')
+                        ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
+                        ->join('images', 'show_images.image_id', '=' ,'images.id')
+                        ->select('images.id','images.url')
+                        ->where('shows.id','=',$id)
+                        ->where('images.image_type','=','Image')
+                        ->distinct()->get();
+            foreach ($images as $i)
+                $i->url = Image::view_image($i->url);
+            foreach ($shows as $s)
+            {
+                $s->showtimes = $showtimes;
+                $s->videos = $videos;
+                $s->images = $images;
+            }                
         }         
         else if(!empty($venue_id) && is_numeric($venue_id))
         {
