06ba8f5e3e7bc7c8aadecb9cc96fb0a3d77f314b
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-03-13 12:04:16 -0700

    ticketbatapp

--- app/Http/Controllers/App/AppController.php
+++ app/Http/Controllers/App/AppController.php
@@ -40,25 +40,35 @@ class AppController extends Controller{
         if(!empty($id) && is_numeric($id))
         {
             $shows = DB::table('shows')
-                    ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
-                    ->join('images', 'show_images.image_id', '=' ,'images.id')
                     ->join('venues', 'venues.id', '=' ,'shows.venue_id')
                     ->join('locations', 'locations.id', '=' ,'venues.location_id')
-                    ->select('shows.id','shows.name','images.url','shows.description','shows.slug')
-                    ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('images.image_type','=','Header')
-                    ->whereNotNull('images.url')->where('shows.id','=',$id)
+                    ->join('show_times', 'shows.id', '=' ,'show_times.show_id')
+                    ->leftJoin('show_images', 'show_images.show_id', '=' ,'shows.id')
+                    ->leftJoin('images', 'show_images.image_id', '=' ,'images.id')
+                    ->select('shows.id','shows.name','images.url','shows.description','shows.slug',
+                             'locations.address','locations.city','locations.state','locations.zip','locations.lat','locations.lng')
+                    ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)
+                    ->where('show_times.show_time','>',\Carbon\Carbon::now())->where('shows.id','=',$id)
+                    ->whereIn('images.image_type',['Header','Logo'])
+                    ->orderBy('shows.name')->groupBy('shows.id')
                     ->distinct()->get(); 
             $showtimes = ShowTime::where('show_id','=',$id)->where('show_time','>',$current)->orderBy('show_time')->take(50)->get(['id','show_time']); 
-            $videos = DB::table('shows')
-                        ->join('show_videos', 'show_videos.show_id', '=' ,'shows.id')
-                        ->join('videos', 'show_videos.video_id', '=' ,'videos.id')
+            $videos = DB::table('videos')
+                        ->join('show_videos', 'show_videos.video_id', '=' ,'videos.id')
+                        ->join('shows', 'show_videos.show_id', '=' ,'shows.id')
                         ->select('videos.id','videos.embed_code')
                         ->where('shows.id','=',$id)
                         ->distinct()->get();
-            $images = DB::table('shows')
-                        ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
-                        ->join('images', 'show_images.image_id', '=' ,'images.id')
-                        ->select('images.id','images.url')
+            foreach ($videos as $v)
+            {
+                $part1 = explode('src="',$v->embed_code);
+                $part2 = explode('"',$part1[1]);
+                $v->embed_code = $part2[0];
+            } 
+            $images = DB::table('images')
+                        ->join('show_images', 'show_images.image_id', '=' ,'images.id')
+                        ->join('shows', 'show_images.show_id', '=' ,'shows.id')
+                        ->select('images.id','images.url','images.image_type')
                         ->where('shows.id','=',$id)
                         ->where('images.image_type','=','Image')
                         ->distinct()->get();
@@ -78,8 +88,10 @@ class AppController extends Controller{
                         ->join('images', 'show_images.image_id', '=' ,'images.id')
                         ->join('venues', 'venues.id', '=' ,'shows.venue_id')
                         ->join('locations', 'locations.id', '=' ,'venues.location_id')
+                        ->join('show_times', 'shows.id', '=' ,'show_times.show_id')
                         ->select('shows.id','shows.venue_id','shows.name','images.url','locations.city')
                         ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('images.image_type','=','Logo')
+                        ->where('show_times.show_time','>',\Carbon\Carbon::now())
                         ->whereNotNull('images.url')->where('venues.id','=',$venue_id)
                         ->orderBy('shows.name')->groupBy('shows.id')
                         ->distinct()->get();
@@ -87,18 +99,21 @@ class AppController extends Controller{
         else
         {
             $shows = DB::table('shows')
-                    ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
-                    ->join('images', 'show_images.image_id', '=' ,'images.id')
-                    ->join('venues', 'venues.id', '=' ,'shows.venue_id')
-                    ->join('locations', 'locations.id', '=' ,'venues.location_id')
-                    ->select('shows.id','shows.venue_id','shows.name','images.url','locations.city')
-                    ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('images.image_type','=','Logo')
-                    ->whereNotNull('images.url')
-                    ->orderBy('shows.name')->groupBy('shows.id')
-                    ->distinct()->get();
+                        ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
+                        ->join('images', 'show_images.image_id', '=' ,'images.id')
+                        ->join('venues', 'venues.id', '=' ,'shows.venue_id')
+                        ->join('locations', 'locations.id', '=' ,'venues.location_id')
+                        ->join('show_times', 'shows.id', '=' ,'show_times.show_id')
+                        ->select('shows.id','shows.venue_id','shows.name','images.url','locations.city')
+                        ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('images.image_type','=','Logo')
+                        ->where('show_times.show_time','>',\Carbon\Carbon::now())
+                        ->whereNotNull('images.url')
+                        ->orderBy('shows.name')->groupBy('shows.id')
+                        ->distinct()->get();
         }    
         foreach ($shows as $s)
-            $s->url = Image::view_image($s->url);
+            if(!empty($s->url))
+                $s->url = Image::view_image($s->url);
         return $shows->toJson();     
     }
     
@@ -111,8 +126,11 @@ class AppController extends Controller{
                     ->join('venue_images', 'venue_images.venue_id', '=' ,'venues.id')
                     ->join('images', 'venue_images.image_id', '=' ,'images.id')
                     ->join('locations', 'locations.id', '=' ,'venues.location_id')
+                    ->join('shows', 'venues.id', '=' ,'shows.venue_id')
+                    ->join('show_times', 'shows.id', '=' ,'show_times.show_id')
                     ->select('venues.id','venues.name','images.url','locations.city')
                     ->where('venues.is_featured','>',0)->where('images.image_type','=','Logo')
+                    ->where('show_times.show_time','>',\Carbon\Carbon::now())
                     ->whereNotNull('images.url')
                     ->orderBy('venues.name')->groupBy('venues.id')
                     ->distinct()->get();
