2d96528377932c607719fb49fbcef92f00826dd8
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-03-22 15:35:19 -0700

    ticketbatapp

--- app/Http/Controllers/App/AppController.php
+++ app/Http/Controllers/App/AppController.php
@@ -175,7 +175,7 @@ class AppController extends Controller{
                         ->join('shows', 'tickets.show_id', '=' ,'shows.id')
                         ->join('packages', 'tickets.package_id', '=' ,'packages.id')
                         ->leftJoin('purchases', 'purchases.ticket_id', '=' ,'tickets.id')
-                        ->select(DB::raw('tickets.id, tickets.retail_price, tickets.processing_fee, tickets.is_default,
+                        ->select(DB::raw('tickets.id, (tickets.retail_price+tickets.processing_fee) AS amount, tickets.is_default,
                                          tickets.package_id, tickets.ticket_type, tickets.ticket_type_class,
                                          (CASE WHEN (packages.title != "None") THEN packages.title ELSE "" END) AS title,
                                          (CASE WHEN (packages.title != "None") THEN packages.description ELSE "" END) AS description,
