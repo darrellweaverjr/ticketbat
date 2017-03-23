9ce8b9b8c73e54455f93a1bf7d65191830eb71b3
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-03-23 11:42:12 -0700

    fix bug for feed

--- app/Http/Controllers/Feed/FeedController.php
+++ app/Http/Controllers/Feed/FeedController.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\App;
+namespace App\Http\Controllers\Feed;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use App\Http\Models\Image;
-use App\Http\Models\ShowTime;
 
 /**
  * Manage Users
@@ -28,7 +27,7 @@ class FeedController extends Controller{
                     ->join('show_times', 'shows.id', '=' ,'show_times.show_id')
                     ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
                     ->join('images', 'show_images.image_id', '=' ,'images.id')
-                    ->select('shows.id','shows.name','images.url','shows.short_description','shows.slug','MIN(tickets.retail_price) AS price')
+                    ->select(DB::raw('shows.id, shows.name, images.url, shows.short_description, shows.slug, MIN(tickets.retail_price) AS price'))
                     ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('show_times.is_active','=',1)
                     ->where('images.image_type','=','Logo')
                     ->where('show_times.show_time','>',\Carbon\Carbon::now())->where('shows.venue_id','=',$venue_id)
