24f0b53efa200ee88004a00a819968197d8e25f5
Ivan <ivan@Nicks-iMac.local>
2017-03-24 10:36:35 -0700

    change slug for feed, show whole url, and external link if it is set

--- app/Http/Controllers/Feed/FeedController.php
+++ app/Http/Controllers/Feed/FeedController.php
@@ -26,7 +26,7 @@ class FeedController extends Controller{
                     ->join('show_times', 'shows.id', '=' ,'show_times.show_id')
                     ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
                     ->join('images', 'show_images.image_id', '=' ,'images.id')
-                    ->select(DB::raw('shows.id, shows.name, images.url, shows.short_description, shows.slug, MIN(tickets.retail_price) AS price, MIN(show_times.show_time) AS show_time'))
+                    ->select(DB::raw('shows.id, shows.name, images.url, shows.short_description, COALESCE(show_times.slug, CONCAT("'.env('IMAGE_URL_OLDTB_SERVER').'/event/",shows.slug)) AS slug, MIN(tickets.retail_price) AS price, MIN(show_times.show_time) AS show_time'))
                     ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('show_times.is_active','=',1)
                     ->where('images.image_type','=','Logo')
                     ->where('show_times.show_time','>',\Carbon\Carbon::now())->where('shows.venue_id','=',$venue_id)
