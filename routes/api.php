b45ce2d95a82fdb961966336c128cd177b3b04d9
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-04-14 13:51:06 -0700

    split routes according with type, removed commands from web and add nickname for them on console, create API rest for apps and other apps with new url

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,21 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+//ADMIN ROUTES FOR APP
+Route::group(['prefix' => 'app','middleware' => 'cors','namespace' => 'App'], function () {
+    //apps config general
+    Route::get('init', 'GeneralController@init');
+    Route::get('shows/{id?}/{venue_id?}', 'GeneralController@shows');
+    Route::get('showtime/{id}', 'GeneralController@showtime');
+    Route::post('contact', 'GeneralController@contact');
+    //apps config session
+    Route::post('purchases', 'SessionController@purchases');
+    Route::post('venues_check', 'SessionController@venues_to_check');
+    Route::post('events_check', 'SessionController@events_to_check');
+    Route::post('purchases_check', 'SessionController@purchases_to_check');
+});
+//ADMIN ROUTES FOR JSON FEED
+Route::group(['prefix' => 'feed','middleware' => 'cors','namespace' => 'Feed'], function () {
+    //feeds config
+    Route::get('events/{venue_id}', 'FeedController@events');
+});
--- routes/console.php
+++ routes/console.php
@@ -1,18 +1,38 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
+//reports
+Artisan::command('ReportManifest', function () {
+    $this->call('Report:manifest');
+});
+Artisan::command('ReportSales {days=1} {onlyadmin=1}', function ($days, $onlyadmin) {
+    $this->call('Report:sales',['days'=>$days,'onlyadmin'=>$onlyadmin]);
+});
+Artisan::command('ReportSalesReceipt {days=1}', function ($days) {
+    $this->call('Report:sales_receipt',['days'=>$days]);
+});
+Artisan::command('ReportFinancial {weeks=0}', function ($weeks) {
+    $this->call('Report:financial',['weeks'=>$weeks]);
+});
+Artisan::command('ReportConsignment', function () {
+    $this->call('Report:consignment');
+});
+//promotions
+Artisan::command('ReportSales {days=7}', function ($days) {
+    $this->call('Promo:announced',['days'=>$days]);
+});
+Artisan::command('ReportSales {days=1} {onlyadmin=1}', function ($days, $onlyadmin) {
+    $this->call('Report:sales',['days'=>$days,'onlyadmin'=>$onlyadmin]);
+});
+//utilities
+Artisan::command('ShoppingcartClean {days=10}', function ($days) {
+    $this->call('Shoppingcart:clean',['days'=>$days]);
+});
+Artisan::command('ShoppingcartRecover {hours=4}', function ($hours) {
+    $this->call('Shoppingcart:recover',['hours'=>$hours]);
+});
+Artisan::command('ContractUpdateTickets', function () {
+    $this->call('Contract:update_tickets');
+});
+Artisan::command('CheckConsignments', function () {
+    $this->call('Consignments:check');
+});
