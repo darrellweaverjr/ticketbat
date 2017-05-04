7a8073cf7c9a0ffd6e41c16c31e754bd3afa5748
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-05-04 08:20:27 -0700

    dont report route exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -24,6 +24,8 @@ class Handler extends ExceptionHandler
         \Illuminate\Database\Eloquent\ModelNotFoundException::class,
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
+        //custom
+        \Illuminate\Routing\RouteCollection::class,
     ];
 
     /**
