025d320886b8f420ad742b22cffa1cbccae876e2
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-05-30 09:26:40 -0700

    skipe check security cps for blob images preview

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,7 +11,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta http-equiv="Content-Security-Policy" content="default-src 'self'; 
-                          img-src 'self' blob:http://admindev.ticketbat.com http://admindev.ticketbat.com {{env('IMAGE_URL_OLDTB_SERVER')}} {{env('IMAGE_URL_AMAZON_SERVER')}} https://d3ofbylanic3d6.cloudfront.net https://s3-us-west-2.amazonaws.com;
+                          img-src 'self' blob: http://admindev.ticketbat.com {{env('IMAGE_URL_OLDTB_SERVER')}} {{env('IMAGE_URL_AMAZON_SERVER')}} https://d3ofbylanic3d6.cloudfront.net https://s3-us-west-2.amazonaws.com;
                           frame-src 'self' https://www.youtube.com https://vimeo.com https://player.vimeo.com;
                           style-src 'self' http://fonts.googleapis.com  'unsafe-inline';
                           font-src 'self' http://fonts.gstatic.com;
