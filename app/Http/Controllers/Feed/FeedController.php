6432063c00fa4de79220fa2146c5e3cd32b8a8dd
Ivan Barriento Clemente <ivan@ticketbat.com>
2017-11-06 10:56:57 -0800

    fix bug to group shows by showtime instead of shows

--- app/Http/Controllers/Feed/FeedController.php
+++ app/Http/Controllers/Feed/FeedController.php
@@ -27,7 +27,7 @@ class FeedController extends Controller{
                     ->join('show_times', 'shows.id', '=' ,'show_times.show_id')
                     ->join('show_images', 'show_images.show_id', '=' ,'shows.id')
                     ->join('images', 'show_images.image_id', '=' ,'images.id')
-                    ->select(DB::raw('shows.id, shows.name, images.url, shows.short_description, COALESCE(show_times.slug, shows.ext_slug, CONCAT("'.env('IMAGE_URL_OLDTB_SERVER').'/event/",shows.slug)) AS slug, MIN(tickets.retail_price) AS price, MIN(show_times.show_time) AS show_time'))
+                    ->select(DB::raw('show_times.id, shows.name, images.url, shows.short_description, COALESCE(show_times.slug, shows.ext_slug, CONCAT("'.env('IMAGE_URL_OLDTB_SERVER').'/event/",shows.slug)) AS slug, MIN(tickets.retail_price) AS price, MIN(show_times.show_time) AS show_time'))
                     ->where('shows.is_active','>',0)->where('shows.is_featured','>',0)->where('show_times.is_active','=',1)
                     ->where('images.image_type','=','Logo')
                     ->where(DB::raw($cutoff_date),'>', \Carbon\Carbon::now())
