8b334a54c75aebfc09b7fe096b3a7cffc9306436
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-03-06 09:21:58 -0800

    rename web name in app.config

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'TicketBat Admin',
+    'name' => 'TicketBat',
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://admin.ticketbat.com'),
+    'url' => env('APP_URL', 'https://www.ticketbat.com'),
 
     /*
     |--------------------------------------------------------------------------
