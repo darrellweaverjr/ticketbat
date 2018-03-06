2839920684401b0a6db9403d48d3a65f468bf4b4
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-03-06 14:47:30 -0800

    update status to chargeback on purchase 18125

---
+++ database/sqls/2018-03-06_manual_chargeback_order18125.sql
@@ -0,0 +1,5 @@
+/*
+this purchase was made in the old usaepay account, it was manually refund by shine@blackstarlv.com, this is just to update the status of the purchase in the system
+*/
+
+update purchases p set p.status='Chargeback' where p.id = 18125;
