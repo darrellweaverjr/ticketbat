7032e1949dbe033f3d993a68027c278084321212
William <william@ticketbat.com>
2018-03-14 16:02:59 -0700

    fix env var for GTM workspace id

--- resources/views/production/general/gtm-body.blade.php
+++ resources/views/production/general/gtm-body.blade.php
@@ -1,4 +1,4 @@
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{env('GA_TRACKING_ID','GTM-KVH4W3K')}}"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{env('GTM_WORKSPACE_ID','GTM-KVH4W3K')}}"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
