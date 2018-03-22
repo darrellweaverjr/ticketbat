28483c57eb49dd52df7214b484666c2544ca4c62
William <william@ticketbat.com>
2018-03-22 10:51:13 -0700

    comment out line

--- database/sqls/2018-03-21_add_mobile_image_enum.sql
+++ database/sqls/2018-03-21_add_mobile_image_enum.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `devticketbat`.`images`
+#ALTER TABLE `images`
 CHANGE COLUMN `image_type` `image_type` ENUM('Logo', 'Image', 'Header', 'Header Medium', 'Mobile Header') NOT NULL DEFAULT 'Image' ;
