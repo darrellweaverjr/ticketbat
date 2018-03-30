46866a02beb10830ec7fbbddb65af4944b58e603
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-03-30 16:13:42 -0700

    pulsar fix some consignments

---
+++ database/sqls/2018-03-30_Pulsar_fix_consignments.sql
@@ -0,0 +1,19 @@
+#c 221
+update purchases set retail_price = 378.00, commission_percent = 0.00, quantity=28, processing_fee=42.00, price_paid=420.00 where id= 18613;
+update seats set retail_price = 13.50, fixed_commission = 0.00, collect_price=15.00, processing_fee= 1.50 where consignment_id= 221;
+
+#c 222
+update purchases set retail_price = 13.50, commission_percent = 0.00, quantity=1, processing_fee=1.50, price_paid=15.00 where id= 18614;
+update seats set retail_price = 13.50, fixed_commission = 0.00, collect_price=15.00, processing_fee= 1.50 where consignment_id= 222;
+
+#c 223
+update purchases set retail_price = 94.50, commission_percent = 0.00, quantity=7, processing_fee=10.50, price_paid=105.00 where id= 18615;
+update seats set retail_price = 13.50, fixed_commission = 0.00, collect_price=15.00, processing_fee= 1.50 where consignment_id= 223;
+
+#c 225
+update purchases set retail_price = 121.50, commission_percent = 0.00, quantity=9, processing_fee=13.50, price_paid=135.00 where id= 18617;
+update seats set retail_price = 13.50, fixed_commission = 0.00, collect_price=15.00, processing_fee= 1.50 where consignment_id= 225;
+
+#c 228
+update purchases set retail_price = 283.50, commission_percent = 0.00, quantity=21, processing_fee=31.50, price_paid=315.00 where id= 18669;
+update seats set retail_price = 13.50, fixed_commission = 0.00, collect_price=15.00, processing_fee= 1.50 where consignment_id= 228;
