f097f11afff6b8c186b20e59bee459c4059c2176
William <william@ticketbat.com>
2018-03-30 12:20:23 -0700

    comment out sql

--- database/sqls/2018-03-29_remove_field_venues_add_pos_fields_fees.sql
+++ database/sqls/2018-03-29_remove_field_venues_add_pos_fields_fees.sql
@@ -1,8 +1,8 @@
 #erase unneeded field on venues
-ALTER TABLE `venues` DROP COLUMN `after_purchase_link`;
+#ALTER TABLE `venues` DROP COLUMN `after_purchase_link`;
 
 #add field to add optional fees when purchasing using POS
-ALTER TABLE `shows`
-ADD COLUMN `pos_fee` DECIMAL(4,2) NULL DEFAULT NULL AFTER `regular_price`;
-ALTER TABLE `venues` 
-ADD COLUMN `pos_fee` DECIMAL(4,2) NULL DEFAULT NULL AFTER `disable_cash_breakdown`;
+#ALTER TABLE `shows`
+#ADD COLUMN `pos_fee` DECIMAL(4,2) NULL DEFAULT NULL AFTER `regular_price`;
+#ALTER TABLE `venues`
+#ADD COLUMN `pos_fee` DECIMAL(4,2) NULL DEFAULT NULL AFTER `disable_cash_breakdown`;
