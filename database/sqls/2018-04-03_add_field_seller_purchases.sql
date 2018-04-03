77f758476bbba7a7898d1f1f9f1f077d8ab8abc7
William <william@ticketbat.com>
2018-04-03 12:20:59 -0700

    queries ran

--- database/sqls/2018-04-03_add_field_seller_purchases.sql
+++ database/sqls/2018-04-03_add_field_seller_purchases.sql
@@ -1,7 +1,7 @@
 #create field seller into purchases to determinate origin of transaction
-ALTER TABLE `purchases`
-ADD COLUMN `seller` ENUM('Web', 'App', 'POS') NOT NULL DEFAULT 'Web' AFTER `note`;
+#ALTER TABLE `purchases`
+#ADD COLUMN `seller` ENUM('Web', 'App', 'POS') NOT NULL DEFAULT 'Web' AFTER `note`;
 
 #change field seller to app or pos system according to purchases
-update purchases p join users u on p.user_id=u.id set p.seller = 'POS' where u.user_type_id = 7 OR u.user_type_id = 1;
-update purchases p set p.seller = 'App' where p.session_id LIKE 'app_%';
+#update purchases p join users u on p.user_id=u.id set p.seller = 'POS' where u.user_type_id = 7 OR u.user_type_id = 1;
+#update purchases p set p.seller = 'App' where p.session_id LIKE 'app_%';
