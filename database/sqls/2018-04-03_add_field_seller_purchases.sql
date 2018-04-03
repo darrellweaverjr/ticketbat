dd5cab8e3ea7fd279a392cb2025f08a806297622
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-04-03 10:19:56 -0700

    add new field flag in purchases to determinate seller origin, app or pos or web

--- database/sqls/2018-04-03_add_field_seller_purchases.sql
+++ database/sqls/2018-04-03_add_field_seller_purchases.sql
@@ -1,7 +1,7 @@
 #create field seller into purchases to determinate origin of transaction
 ALTER TABLE `purchases`
-ADD COLUMN `seller` VARCHAR(1) NOT NULL DEFAULT 'W' AFTER `note`;
+ADD COLUMN `seller` ENUM('Web', 'App', 'POS') NOT NULL DEFAULT 'Web' AFTER `note`;
 
 #change field seller to app or pos system according to purchases
-update purchases p join users u on p.user_id=u.id set p.seller = 'P' where u.user_type_id = 7;
-update purchases p set p.seller = 'A' where p.session_id LIKE 'app_%';
+update purchases p join users u on p.user_id=u.id set p.seller = 'POS' where u.user_type_id = 7;
+update purchases p set p.seller = 'App' where p.session_id LIKE 'app_%';
