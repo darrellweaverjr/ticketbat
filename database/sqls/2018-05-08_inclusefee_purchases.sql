a07ddeed03da48a8a1b7c163b8e6ec483be3b595
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-05-08 15:32:17 -0700

    update old rows inclusive fee for purchases

--- database/sqls/2018-05-08_inclusefee_purchases.sql
+++ database/sqls/2018-05-08_inclusefee_purchases.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `purchases` 
 ADD COLUMN `inclusive_fee` TINYINT(1) NOT NULL DEFAULT '0' AFTER `channel`;
+
+update purchases p inner join tickets t on p.ticket_id=t.id
+set p.inclusive_fee = 1 where t.inclusive_fee>0 AND p.id>0;
