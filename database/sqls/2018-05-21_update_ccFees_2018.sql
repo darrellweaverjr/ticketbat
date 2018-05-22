d823e662e8192a92cf37a9b13dc4de60a5ec6e9f
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-05-22 14:02:49 -0700

    update cc_fee for purchases of shows in this year

--- database/sqls/2018-05-21_update_ccFees_2018.sql
+++ database/sqls/2018-05-21_update_ccFees_2018.sql
@@ -1,4 +1,13 @@
+#all purchases made this year
 update purchases p 
 inner join transactions t on p.transaction_id = t.id
 set p.cc_fees = round(0.03*t.amount,2)
 where t.trans_result='Approved' and p.payment_type='Credit' and date(p.created)>='2018-01-01';
+
+
+#with the shows in this year
+update purchases p 
+inner join transactions t on p.transaction_id = t.id 
+inner join show_times st on st.id = p.show_time_id
+set p.cc_fees = round(0.03*t.amount,2)
+where t.trans_result='Approved' and p.payment_type='Credit' and date(st.show_time)>='2018-01-01';
