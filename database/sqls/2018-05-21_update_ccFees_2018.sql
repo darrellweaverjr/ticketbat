db7b96c26b2af0b1708c7e1b890366c063507769
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-05-22 17:02:34 -0700

    add query to fix accounting issues

--- database/sqls/2018-05-21_update_ccFees_2018.sql
+++ database/sqls/2018-05-21_update_ccFees_2018.sql
@@ -11,3 +11,9 @@ inner join transactions t on p.transaction_id = t.id
 inner join show_times st on st.id = p.show_time_id
 set p.cc_fees = round(0.03*t.amount,2)
 where t.trans_result='Approved' and p.payment_type='Credit' and date(st.show_time)>='2018-01-01';
+
+update purchases p 
+inner join transactions t on p.transaction_id = t.id 
+inner join show_times st on st.id = p.show_time_id
+set p.cc_fees = round(0.03*p.price_paid,2)
+where t.trans_result='Approved' and p.payment_type='Credit' and date(st.show_time)>='2018-01-01';
