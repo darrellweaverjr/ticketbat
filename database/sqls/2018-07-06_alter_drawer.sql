5e05535009ad0eae179d5f0b370c0681d6d5a3b0
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-07-06 09:30:54 -0700

    fix bug with drawer

--- database/sqls/2018-07-06_alter_drawer.sql
+++ database/sqls/2018-07-06_alter_drawer.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `ticketba_lvtn`.`user_seller` 
 CHANGE COLUMN `open_drawer` `open_drawer` INT(10) NOT NULL DEFAULT '0' ;
+
+ALTER TABLE `ticketba_lvtn`.`seller_tally` 
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`id`);
