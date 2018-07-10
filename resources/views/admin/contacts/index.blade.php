8212524a747afee610ddbaa35dc68e7d4e7b6f28
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-07-10 08:42:28 -0700

    changes on Contacts view

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -39,21 +39,24 @@
                     <table class="table table-striped table-bordered table-hover table-checkable" id="tb_model">
                         <thead>
                             <tr>
-                                <th width="8%"> Date </th>
-                                <th width="17%"> Client </th>
-                                <th width="75%"> Message </th>
+                                <th width="7%">ID</th>
+                                <th width="8%">Date</th>
+                                <th width="15%">Client</th>
+                                <th width="70%">Message</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($contacts as $index=>$c)
                             <tr>
-                                <td width="8%" data-order="{{strtotime($c->created)}}"> {{date('m/d/Y',strtotime($c->created))}} <br> {{date('g:ia',strtotime($c->created))}} </td>
-                                <td width="17%"> <b class="uppercase"><a href="mailto:{{$c->email}}" target="_top">{{$c->name}}</a></b>
+                                <td class="text-center">{{$c->id}}</td>
+                                <td class="text-center">{{date('m/d/Y g:ia',strtotime($c->created))}}</td>
+                                <td>
+                                    <b class="uppercase"><a href="mailto:{{$c->email}}" target="_top">{{$c->name}}</a></b>
                                     <p><small><i>
                                         @if($c->email)Email: <a href="mailto:{{$c->email}}" target="_top">{{$c->email}}</a> <br>@endif
                                         @if($c->phone)Phone: <b>{{$c->phone}}</b><br>@endif
                                     </i></small></p>
-                                <td width="75%">
+                                <td>
                                     <p><small><i>
                                         @if($c->show_name)Show: <b>{{$c->show_name}}</b> @if($c->show_time) - Date/Time: <b>{{date('l, F jS, Y h:i A',strtotime($c->show_time))}}</b> @endif <br>@endif
                                         @if($c->system_info)System: <b>{{$c->system_info}}</b>@endif
