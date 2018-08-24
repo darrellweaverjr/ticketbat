64cd908a1d4636e6683fce6e8669342f209388b7
Ivan Barriento Clemente <ivan@ticketbat.com>
2018-08-24 09:23:10 -0700

    fix bug on templates

--- public/js/utils/templates.js
+++ public/js/utils/templates.js
@@ -16,38 +16,41 @@ var MainFormValidation = function () {
         // for more info visit the official plugin documentation: 
         // http://docs.jquery.com/Plugins/Validation
         var form = $('#'+form_id);
-        var error = $('.alert-danger', form);
-        var success = $('.alert-success', form);
-        //VALIDATOR
-        form.validate({
-            errorElement: 'span', //default input error message container
-            errorClass: 'help-block help-block-error', // default input error message class
-            focusInvalid: false, // do not focus the last invalid input
-            ignore: "", // validate all fields including form hidden input
-            rules: rules,
-            invalidHandler: function (event, validator) { //display error alert on form submit   
-                success.hide();
-                error.show();
-                App.scrollTo(error, -200);
-            },
-            highlight: function (element) { // hightlight error inputs
-               $(element)
-                    .closest('.show-error').addClass('has-error'); // set error class to the control group
-            },
-            unhighlight: function (element) { // revert the change done by hightlight
-                $(element)
-                    .closest('.show-error').removeClass('has-error'); // set error class to the control group
-            },
-            success: function (label) {
-                label
-                    .closest('.show-error').removeClass('has-error'); // set success class to the control group
-            },
-            submitHandler: function (form) {
-                success.show();
-                error.hide();
-                form[0].submit(); // submit the form
-            }
-        }).showErrors(showErrors);
+        if(form.length>=1)
+        {
+            var error = $('.alert-danger', form);
+            var success = $('.alert-success', form);
+            //VALIDATOR
+            form.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "", // validate all fields including form hidden input
+                rules: rules,
+                invalidHandler: function (event, validator) { //display error alert on form submit   
+                    success.hide();
+                    error.show();
+                    App.scrollTo(error, -200);
+                },
+                highlight: function (element) { // hightlight error inputs
+                   $(element)
+                        .closest('.show-error').addClass('has-error'); // set error class to the control group
+                },
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.show-error').removeClass('has-error'); // set error class to the control group
+                },
+                success: function (label) {
+                    label
+                        .closest('.show-error').removeClass('has-error'); // set success class to the control group
+                },
+                submitHandler: function (form) {
+                    success.show();
+                    error.hide();
+                    form[0].submit(); // submit the form
+                }
+            }).showErrors(showErrors);
+        }
     }
     
     return {
