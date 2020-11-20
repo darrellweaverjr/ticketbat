a7ba85504d6979e8e1d69ae1b00948911ae8f574
Darrell Weaver <darrellweaverjr@gmail.com>
2020-11-20 14:19:25 -0800

    added google tag

--- resources/views/layouts/production.blade.php
+++ resources/views/layouts/production.blade.php
@@ -88,6 +88,15 @@
           });
         </script>
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FNVPT3E3Y1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FNVPT3E3Y1');
+        </script>
 
         <script type="text/javascript">
             adroll_adv_id = "L2DZJ45JL5BEZKARYKAIJR";
